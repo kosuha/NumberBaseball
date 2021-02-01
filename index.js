1a0c36ac21b271141d5be6b02bb9529e00e5f83b
Seonho Kim <zxzx235@naver.com>
2021-02-01 19:18:35 +0900

    입력에 중복된 숫자가 있는 경우

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /*
 TODO
-입력에 중복된 숫자가 있는 경우
+입력에 중복된 숫자가 있는 경우(완료)
 10회 안에 정답을 못 맞춘 경우
 정답 맞춘 경우
 CSS
@@ -15,22 +15,30 @@ const wrongInputAlert = document.querySelector('#wrongInputAlert')
 
 let count = 10
 const numbers = getRandomNumber()
-countText.textContent = '도전 가능 횟수가 ' + count + '회 남았습니다.' 
+countText.textContent = '도전 가능 횟수가 ' + count + '회 남았습니다.'
 
 enter.addEventListener('click', () => {
-    if (input.value && (input.value.length === 4)) {
+    if (isValidNumber(input.value)) {
         compare(input.value)
         let newLog = document.createElement('div')
         logs.prepend(newLog)
-        newLog.textContent = compare(input.value)[0] + 'S' + compare(input.value)[1] + 'B'
-        
+        if (compare(input.value)[0] === 4) {
+            newLog.textContent = '승리!'
+        } else {
+            newLog.textContent = compare(input.value)[0] + 'S' + compare(input.value)[1] + 'B'
+        }
         wrongInputAlert.textContent = ''
+
     } else {
         wrongInputAlert.textContent = '잘못된 입력입니다.'
     }
 
     count--
     countText.textContent = '도전 가능 횟수가 ' + count + '회 남았습니다.'
+    
+    if (count === 0) {
+        countText.textContent = '패배...'
+    }
 })
 
 function getRandomNumber() {
@@ -60,4 +68,21 @@ function compare(inputValue) {
     }
 
     return [strike, ball]
+}
+
+function isValidNumber(value) {
+    if (!value) {
+        return false
+    }
+    if (value.length !== 4) {
+        return false
+    }
+    for (let index = 0; index < value.length; index++) {
+        const regexp = new RegExp(value[index], 'g')
+        if (value.match(regexp).length > 1) {
+            console.log(value.match(regexp));
+            return false
+        }
+    }
+    return true
 }
